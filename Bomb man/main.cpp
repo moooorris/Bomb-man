9b20688e0ff593ab2219d8bf9b99b86eeaaadafd
Wen Yu Chang <morris.chang@ecloudvalley.com>
2019-12-10 15:32:26 +0800

    Initial Commit

---
+++ Bomb man/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Bomb man
+//
+//  Created by 張文毓 on 2019/12/10.
+//  Copyright © 2019 張文毓. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
